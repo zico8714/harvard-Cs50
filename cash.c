c73501af2d75c7247b3f618317a75387bd87bd55
zico8714 <zico8714@users.noreply.github.com>
2020-03-19 03:16:56 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <math.h>
 
 int main(void)
 {
-    int o = get_float("What do I owe you?\n");
-    printf("%i", o);
+    int coins = 0;
+    float o = 0.0;
 
+    
+    //Prompt user to get float
+    do
+    {
+    o = get_float("What do I owe you?\n");      
+    }
+    while(o < 0);
+    
+    int Round = round(o * 100);
+    do
+    {
+    if (Round >= 25)
+        {
+            Round -= 25;
+            coins++;
+        }
+        else if (Round >= 10)
+        {
+            Round -= 10;
+            coins++;
+        }
+        else if (Round >= 5)
+        {
+            Round -= 5;
+            coins++;
+        }
+        else
+        {
+            Round -= 1;
+            coins++;
+        }
+    }while(Round > 0);
+        printf("%i\n", coins);
 }
