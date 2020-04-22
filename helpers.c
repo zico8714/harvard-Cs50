5f4be53d01ce4d12d4c6044d1d1a98b39d2fdbcc
zico8714 <zico8714@users.noreply.github.com>
2020-04-22 20:55:37 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -38,12 +38,86 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    //Set a float to get the middle of the
+    float phase = width / 2;
+
+    for (int i = 0; i < height; i++)
+    {
+        //Set counter to number of pixels -1
+        int count = width - 1;
+        for (int j = 0; j < phase; j++)
+        {
+            if (image[i][j].rgbtRed != image[i][count].rgbtRed || image[i][j].rgbtBlue != image[i][count].rgbtBlue
+            || image[i][j].rgbtGreen != image[i][count].rgbtGreen)
+            {
+                RGBTRIPLE tmp;
+                tmp = image[i][count];
+                image[i][count] = image[i][j];
+                image[i][j] = tmp;
+                count--;
+            }
+            else
+            {
+                count--;
+                break;
+            }
+        }
+    }
     return;
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE image2[height][width];
+
+    for (int i  = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float sumB = 0;
+            float sumG = 0;
+            float sumR = 0;
+            int count = 0;
+            for (int h = i - 1; h < i + 2; h++)
+            {
+                for (int w = j - 1; w < j + 2; w++)
+                {
+                    if (h >= 0 && w >= 0 && h < height && w < width)
+                    {
+                        sumB += image[h][w].rgbtBlue;
+                        sumG += image[h][w].rgbtGreen;
+                        sumR += image[h][w].rgbtRed;
+                        count++;
+                    }
+                }
+            }
+            //Barbi te amo
+            float avgB = sumB / count;
+            float avgG = sumG / count;
+            float avgR = sumR / count;
+
+            int B = round(avgB);
+            int G = round(avgG);
+            int R = round(avgR);
+
+            image2[i][j].rgbtBlue = B;
+            image2[i][j].rgbtGreen = G;
+            image2[i][j].rgbtRed = R;
+        }
+    }
+    //Barbi te amooooooooooooooooooooooooo
+    for (int a = 0; a < height; a++)
+    {
+        for (int l = 0; l < width; l++)
+        {
+            image[a][l] = image2[a][l];
+        }
+    }
+    //BARBI TE AMO
+//BARBI TE AMO
+//BARBI TE AMO
+//BARBI TE AMO
     return;
 }
 
