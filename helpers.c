0ced1c51f2cdac6e594f13baec66e3f34e4ebdca
zico8714 <zico8714@users.noreply.github.com>
2020-04-22 20:52:50 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,4 +1,7 @@
 #include "helpers.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
