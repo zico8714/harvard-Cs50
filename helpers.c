73d34ffe6ed76694f818c610a712c31cf859f6d0
zico8714 <zico8714@users.noreply.github.com>
2020-04-23 22:50:32 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -115,14 +115,170 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         }
     }
     //BARBI TE AMO
-//BARBI TE AMO
-//BARBI TE AMO
-//BARBI TE AMO
+    //BARBI TE AMO
+    //BARBI TE AMO
+    //BARBI TE AMO
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    //Create a second base
+    RGBTRIPLE image2[height][width];
+
+    //Get every pixel
+    for (int i  = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int GxsumB;
+            int GxsumG;
+            int GxsumR;
+            int GysumB;
+            int GysumG;
+            int GysumR;
+
+            //Get every 3x3 grid
+            //Gx
+            for (int hGx = i - 1; hGx < i + 2; hGx++)
+            {
+                for (int wGx = j - 1; wGx < j + 2; wGx++)
+                {
+                    if (hGx < 0 || wGx < 0 || hGx > height || wGx > width || wGx == j)
+                    {
+                        GxsumB += 0;
+                        GxsumG += 0;
+                        GxsumR += 0;
+                    }
+                    else if (wGx == j - 1)
+                    {
+                        if (hGx != i)
+                        {
+                            GxsumB += image[hGx][wGx].rgbtBlue * -1;
+                            GxsumG += image[hGx][wGx].rgbtGreen * -1;
+                            GxsumR += image[hGx][wGx].rgbtRed * -1;
+                        }
+                        else
+                        {
+                            GxsumB += image[hGx][wGx].rgbtBlue * -2;
+                            GxsumG += image[hGx][wGx].rgbtGreen * -2;
+                            GxsumR += image[hGx][wGx].rgbtRed * -2;
+                        }
+                    }
+                    else if (wGx == j + 1)
+                    {
+                        if (hGx != i)
+                        {
+                            GxsumB += image[hGx][wGx].rgbtBlue * 1;
+                            GxsumG += image[hGx][wGx].rgbtGreen * 1;
+                            GxsumR += image[hGx][wGx].rgbtRed * 1;
+                        }
+                        else
+                        {
+                            GxsumB += image[hGx][wGx].rgbtBlue * 2;
+                            GxsumG += image[hGx][wGx].rgbtGreen * 2;
+                            GxsumR += image[hGx][wGx].rgbtRed * 2;
+                        }
+                    }
+                }
+
+                if (GxsumB > 255)
+                {
+                    GxsumB = 255;
+                }
+
+                if (GxsumG > 255)
+                {
+                    GxsumG = 255;
+                }
+
+                if (GxsumR > 255)
+                {
+                    GxsumR = 255;
+                }
+            }
+            //Gy
+            for (int hGy = i - 1; hGy < i + 2; hGy++)
+            {
+                for (int wGy = j - 1; wGy < j + 2; wGy++)
+                {
+                    if (hGy < 0 || wGy < 0 || hGy > height || wGy > width || hGy == i)
+                    {
+                        GysumB += 0;
+                        GysumG += 0;
+                        GysumR += 0;
+                    }
+                    else if (hGy == i - 1)
+                    {
+                        if (wGy != j)
+                        {
+                            GysumB += image[hGy][wGy].rgbtBlue * -1;
+                            GysumG += image[hGy][wGy].rgbtGreen * -1;
+                            GysumR += image[hGy][wGy].rgbtRed * -1;
+                        }
+                        else
+                        {
+                            GysumB += image[hGy][wGy].rgbtBlue * -2;
+                            GysumG += image[hGy][wGy].rgbtGreen * -2;
+                            GysumR += image[hGy][wGy].rgbtRed * -2;
+                        }
+                    }
+                    else if (hGy == i + 1)
+                    {
+                        if (wGy != j)
+                        {
+                            GysumB += image[hGy][wGy].rgbtBlue * 1;
+                            GysumG += image[hGy][wGy].rgbtGreen * 1;
+                            GysumR += image[hGy][wGy].rgbtRed * 1;
+                        }
+                        else
+                        {
+                            GysumB += image[hGy][wGy].rgbtBlue * 2;
+                            GysumG += image[hGy][wGy].rgbtGreen * 2;
+                            GysumR += image[hGy][wGy].rgbtRed * 2;
+                        }
+                    }
+                }
+                if (GysumB > 255)
+                {
+                    GysumB = 255;
+                }
+
+                if (GysumG > 255)
+                {
+                    GysumG = 255;
+                }
+
+                if (GysumR > 255)
+                {
+                    GysumR = 255;
+                }
+            }
+            int powGxB = pow(GxsumB, 2);
+            int powGxG = pow(GxsumG, 2);
+            int powGxR = pow(GxsumR, 2);
+
+            int powGyB = pow(GysumB, 2);
+            int powGyG = pow(GysumG, 2);
+            int powGyR = pow(GysumR, 2);
+
+            float sqB = sqrt(powGxB + powGyB);
+            float sqG = sqrt(powGxG + powGyG);
+            float sqR = sqrt(powGxR + powGyR);
+
+            image2[i][j].rgbtBlue = round(sqB);
+            image2[i][j].rgbtGreen = round(sqG);
+            image2[i][j].rgbtRed = round(sqR);
+        }
+    }
+
+    for (int a = 0; a < height; a++)
+    {
+        for (int l = 0; l < width; l++)
+        {
+            image[a][l] = image2[a][l];
+        }
+    }
     return;
 }
