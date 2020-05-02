7e86aac953c93028bf726a2a4e6a3937aee4bf41
zico8714 <zico8714@users.noreply.github.com>
2020-05-02 20:28:10 +0000

    automated commit by check50 [check50=True]

---
+++ recover.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+
+
+    if (argc != 2)
+    {
+        printf("Usage: ./recover incard\n");
+        return 1;
+    }
+
+    char *inc = argv[1];
+    char *outimg = malloc(sizeof(BYTE) * 512);
+
+    // Open input card
+    FILE *inptr = fopen(inc, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", inc);
+        return 1;
+    }
+
+    int countV = 0;
+    int count = 0;
+
+
+    while (!feof(inptr))
+    {
+        BYTE buffer[512];
+        int x = fread(buffer, sizeof(BYTE), 512, inptr);
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            //close previous file
+            count++;
+            sprintf(outimg, "%03i.jpg", count);
+            FILE* outptr = fopen(outimg, "w");
+            fwrite(buffer, sizeof(BYTE), 512, outptr);
+            fclose(outptr);
+        }
+        else if (count == 0)
+        {
+            countV++;
+        }
+        else
+        {
+            FILE* outptr = fopen(outimg, "a");
+            fseek(outptr, 0, SEEK_END);
+            fwrite(buffer, sizeof(BYTE), 512, outptr);
+            fclose(outptr);
+        }
+    }
+
+    fclose(inptr);
+}
